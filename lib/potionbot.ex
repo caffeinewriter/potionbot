c3fa297c696007dce86d25c732a1911e74b361af
Brandon Anzaldi <brandon@brandonanzaldi.com>
2018-02-22 03:06:32 -0800

    Initial commit

---
+++ lib/potionbot.ex
@@ -0,0 +1,18 @@
+defmodule Potionbot do
+  @moduledoc """
+  Documentation for Potionbot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Potionbot.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
